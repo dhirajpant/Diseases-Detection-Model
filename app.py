72a962eb8da1fb67b5ab3bf625211d548ab554c9
dhirajpant <dhirajpant.ad@gmail.com>
2024-06-25 07:41:05 +0545

    updated model

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import google.generativeai as gen_ai
 load_dotenv()
 
 # Load the trained model
-model = keras.models.load_model("detection_model.h5")
+model = keras.models.load_model("detection_model_mobilenetv2.h5")
 
 # Load class indices from JSON file
 class_indices = json.load(open("class_indices.json"))
@@ -23,7 +23,7 @@ gemini_model = gen_ai.GenerativeModel('gemini-pro')
 # Function to preprocess image
 def preprocess_image(image):
     image_rgb = image.convert("RGB")
-    resized_image = image_rgb.resize((256, 256))
+    resized_image = image_rgb.resize((224, 224))
     normalized_image = np.array(resized_image, dtype=np.float32) / 255.0
     input_image = np.expand_dims(normalized_image, axis=0)
     return input_image
@@ -31,10 +31,12 @@ def preprocess_image(image):
 # Function to predict diseases
 def predict_diseases(model, image, class_indices):
     preprocessed_img = preprocess_image(image)
+    probs = model.predict(preprocessed_img)[0]
+    pred_class_prob = np.argmax(probs)
     predictions = model.predict(preprocessed_img)
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    predicted_class_index = np.argmax(predictions)
     predicted_class_name = class_indices[str(predicted_class_index)]
-    return predicted_class_name
+    return predicted_class_name,probs[pred_class_prob]
 
 st.title("Plants Diseases Classifier with AI")
 st.subheader("Made by Vecrosoft")
@@ -48,8 +50,8 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Predict"):
-        predicted_disease = predict_diseases(model, image, class_indices)
-        st.success(f'Predicted Disease: {predicted_disease}')
+        predicted_disease,probability = predict_diseases(model, image, class_indices)
+        st.success(f'Predicted Disease: {predicted_disease} \n\n\n Probability: {probability:.2f}')
         if "Healthy" not in predicted_disease:
         # Ask Gemini for information
             with st.spinner("Generating Information"):
